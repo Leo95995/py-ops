708a25457f859ce53f1a9acff1c5ca2a57294975
Leo95995 <malvolti68@gmail.com>
2026-01-15 22:37:33 +0100

    fix(backup-manager): added aws bucket upload

--- backup-manager/backup_manager.py
+++ backup-manager/backup_manager.py
@@ -34,33 +34,65 @@ def create_tar_file(source: str, target: str):
         try:
             with tarfile.open(f"./{target}", mode="w:gz") as tar_file:
                 tar_file.add(source, arcname=os.path.basename(source))
-            return True
+            return {"status":True, "file": target } 
         except Exception as e:
             print(f"Error during the file compression: {e}")
-            return False
+            return {"status":False}
     else:
         raise Exception('Invalid source Path. Try again')
   
 
 # function used to upload the backup on S3 bucket
-def s3_upload():    
-    print('Uploading on S3')
+"""
+Cosa gli passi come target? 
+Come target gli vai a passare la stringa che rappresenta il file/folder 
+da caricare sul bucket
+"""
+def s3_upload(target: str):    
+    # primo parametro la variabile, secondo parametro -> default
+    bucket = os.getenv("S3_BUCKET_NAME")
+    access_key = os.getenv("AWS_ACCESS_KEY_ID")
+    secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+    region = os.getenv("AWS_REGION","eu-central-1")
+
+    if not all([bucket, access_key, secret_key]):
+        return False
+    
+    s3 = boto3.client("s3", 
+                      aws_access_key_id=access_key,
+                      aws_secret_access_key=secret_key,
+                      region_name=region)
+    try: 
+        print(f"Upload di {target} su S3 (Bucket: {bucket})...")
+        s3.upload_file(target, bucket, target)
+        return True
+    except Exception as e: 
+        print(e+ 'Errore')
+        return False
+
 
 
 def main():
     args=get_args()
     source=args.source
+    if not source: 
+        print("You must select  source with -s")
+        return
+
     # get the output file clean name
     output=prepare_output_source(args.output)
     # generate the backup tar file
-    create_tar_file(source, output)
+    tar_res = create_tar_file(source, output)
 
-    # creating tar file
-
-   
-
-    # upload on s3..
-    s3_upload()
+    if tar_res["status"]:
+        file_generato = tar_res["file"]
+        if s3_upload(file_generato):
+          print(f"Backup of {file_generato} completed and uploaded")
+          os.remove(file_generato)
+        else:
+            print("Failed Upload")
+    else:
+        print("Compression failed. operation aborted")
 
 
 if __name__ == '__main__':
